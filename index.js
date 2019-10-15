561a4a34568147fecf063de9246f2059723ac2ac
vip-seo <55869630+vip-seo@users.noreply.github.com>
2019-10-15 15:13:59 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const timer = require('./app');
 
 console.log(new Date().toString('dd.MM.yyyy hh:mm'));
-console.log(timer.addInterval(10,1,0,1,1));
+console.log(timer.addInterval(10,1,0,1,90)); //(years, months, days, hours, minutes)
 console.log(timer.subtInterval(10,1,0,1,1));
