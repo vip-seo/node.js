c61d2b78f6b5988ef17a3d645d784773fb1af2f9
vip-seo <55869630+vip-seo@users.noreply.github.com>
2019-10-15 15:14:23 +0300

    Update app.js

--- app.js
+++ app.js
@@ -3,9 +3,9 @@
 module.exports.addInterval = function (years, months, days, hours, minutes) {
     var today = new Date();
     var future, newYear, newMonth, newDay, newHour, newMinute;
-
+        
     newYear = years ? today.getFullYear() + years : today.getFullYear();
-    newMonth =  months ? today.getMonth() + months : today.getMonth();
+    newMonth = months ? today.getMonth() + months : today.getMonth();
     newDay = days ? today.getDate() + days : today.getDate();
     newHour = hours ? today.getHours() + hours : today.getHours();
     newMinute = minutes ? today.getMinutes() + minutes : today.getMinutes();
@@ -20,16 +20,10 @@ module.exports.subtInterval = function (years, months, days, hours, minutes) {
     var past, newYear, newMonth, newDay, newHour, newMinute;
 
     newYear = years ? today.getFullYear() - years : today.getFullYear();
-    newMonth =  months ? today.getMonth() - months : today.getMonth();
+    newMonth = months ? today.getMonth() - months : today.getMonth();
     newDay = days ? today.getDate() - days : today.getDate();
     newHour = hours ? today.getHours() - hours : today.getHours();
     newMinute = minutes ? today.getMinutes() - minutes : today.getMinutes();
     past = new Date(newYear, newMonth, newDay, newHour, newMinute);
     return past.toString('dd.MM.yyyy hh:mm');
-};
-
-//Вычитание двух временных интервалов (dd.mm.yyyy, dd.mm.yyyy)
-//
-module.exports.diffInterval = function (interval1, interval2) {
-    
 };
