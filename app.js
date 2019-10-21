079c15f1010a386c97c019c83e001f819402ed43
vip-seo <55869630+vip-seo@users.noreply.github.com>
2019-10-21 21:52:47 +0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,36 @@
+const express = require('express');
+const port = 3000;
+const app = express();
+
+const courses = [
+    {
+        id: 1,
+        name: 'Node.js'
+    },
+    {
+        id: 2,
+        name: 'Angular'
+    },
+    {
+        id: 3,
+        name: 'JQuery'
+    }
+];
+
+app.get('/', function (req, res) {
+    res.send('Hello world');
+});
+app.get('/api/courses', function (req, res) {
+    res.send(courses);
+});
+app.listen(port, function () {
+    console.log('Server at http://localhost:3000');
+});
+
+
+//CRUD
+//--------------------------
+//CREATE - POST
+//READ - GET
+//UPDATE - UPDATE
+//DELETE - DELETE
