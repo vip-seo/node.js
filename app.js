94cbc99a9261862f91e9020a286189098ff7898f
vip-seo <55869630+vip-seo@users.noreply.github.com>
2019-10-22 17:49:47 +0300

    Update app.js

--- app.js
+++ app.js
@@ -1,6 +1,13 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 const port = 3000;
 const app = express();
+
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }))
+ 
+// parse application/json
+app.use(bodyParser.json())
 
 const courses = [
     {
@@ -20,9 +27,28 @@ const courses = [
 app.get('/', function (req, res) {
     res.send('Hello world');
 });
+
 app.get('/api/courses', function (req, res) {
     res.send(courses);
 });
+
+app.get('/api/courses/:id', function (req, res) {
+    //console.log(typeof req.params.id);
+    let course = courses.find(function(course){
+        return course.id === parseInt(req.params.id);
+    });
+    res.send(course);
+});
+
+app.post('/api/courses', function(req, res){
+    let course = {
+        id: Date.now(),
+        name: req.body.name
+    }
+    courses.push(course);
+    res.send(courses);
+});
+
 app.listen(port, function () {
     console.log('Server at http://localhost:3000');
 });
